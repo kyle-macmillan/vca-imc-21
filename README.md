75fbda76e1ab7b6c86f24bcc2a97d556c9592fc8
Kyle MacMillan <macmillan@uchicago.edu>
2021-09-27 16:19:47 -0500

    edited readme

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ applications: Google Meet, Zoom, and Microsoft Teams. This code relies on
 the python [guibot](https://guibot.readthedocs.io/en/latest/README.html)
 module. We have included images used by guibot to interact with the 
 applications. 
+
+## Sample Usage
+In our experiments, we shaped the interface on the router instead of on the 
+devices. As a result, `static.sh` is configured to communicate with the 
+router. It is possible to configure `static.sh` to shape on the device by 
+changing 
--- static.sh
+++ static.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-TRACE=$1
-ROUTER=$2
+APP=$1
+TIME=$2
+INTERFACE=$3
+TRACE=$4
+ROUTER=$5
+URL=$6
+
 
 echo "Reading trace $TRACE"
 
@@ -15,7 +20,7 @@ do
 		a=( $line )
 		echo "Setting ${a[1]} down and ${a[2]} up"
 		ssh -n $ROUTER "./shaper.sh start ${a[1]} ${a[2]} 0 0"
-		python3 test.py teams 150 -i en4 -id https://teams.microsoft.com/l/meetup-join/19%3a9f85bdc95a344cd29c346f51f3c6be29%40thread.tacv2/1632759553702?context=%7b%22Tid%22%3a%2283b02c92-5f26-48ed-9e5b-6c2fca46a8e6%22%2c%22Oid%22%3a%22099d321a-faad-4c74-9352-3da45ada8fcf%22%7d -r ${a[1]}-${a[2]} 
+		python3 test.py $APP $TIME -i $INTERFACE -id $URL ${a[1]}-${a[2]} 
 		sleep 1
 		ssh -n $ROUTER "./shaper.sh stop"
 		
